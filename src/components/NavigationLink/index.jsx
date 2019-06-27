dee02698976d961295e8a809feb05de653af6a80
Mikael Gramont <mgramont@gmail.com>
2019-06-27 22:31:30 +0200

    Fixed LHS icons

--- src/components/NavigationLink/index.jsx
+++ src/components/NavigationLink/index.jsx
@@ -3,34 +3,14 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-import Album from 'Images/picture-stack-landscape.svg';
-import Hot from 'Images/trends-hot-flame.svg';
-import Users from 'Images/single-neutral-id-card-double.svg';
-import SVG from 'Components/SVG';
-import icons from 'Utils/icons';
+import { getIcon } from 'Utils/icons';
 
 import styles from './styles.scss';
-
-const getIcon = (type) => {
-  switch (type) {
-    case icons.ALBUM:
-      return <SVG icon={Album} label="" className={classnames(styles.defaultIcon)} />;
-
-    case icons.HOT:
-      return <SVG icon={Hot} label="" className={classnames(styles.defaultIcon)} />;
-
-    case icons.USERS:
-      return <SVG icon={Users} label="" className={classnames(styles.defaultIcon)} />;
-
-    default:
-      return null;
-  }
-};
 
 const NavigationLink = ({ active, className, icon, label, to }) => {
   return (
     <Link to={to} className={classnames(className, styles.wrapper, { [styles.active]: active })}>
-      <span>{getIcon({type: icon})}</span>
+      <span className={styles.defaultIcon}>{getIcon({ type: icon })}</span>
       <span>{label}</span>
     </Link>
   );
--- src/utils/icons.jsx
+++ src/utils/icons.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+import SVG from 'Components/SVG';
 import Activity from 'Images/alarm-bell.svg';
 import Album from 'Images/picture-stack-landscape-bold.svg';
 import Close from 'Images/close.svg';
+import Hot from 'Images/trends-hot-flame.svg';
 import Photo from 'Images/camera-bold.svg';
 import Video from 'Images/go-pro-bold.svg';
 import ThreeDots from 'Images/navigation-menu-horizontal.svg';
 import Profile from 'Images/single-neutral-circle-alternate-bold.svg';
 import Search from 'Images/search-alternate-bold.svg';
-import SVG from 'Components/SVG';
-import PropTypes from 'prop-types';
+import Users from 'Images/single-neutral-id-card-double.svg';
 
 const ACTIVITY = 'activity';
 const ALBUM = 'album';
@@ -35,7 +37,16 @@ const icons = {
   VIDEO,
 };
 
-export const getIcon = ({type, label, className, standardIcon = false, presentationOnly = false, hollow = true}) => {
+export default icons;
+
+export const getIcon = ({
+  type,
+  label,
+  className,
+  standardIcon = false,
+  presentationOnly = false,
+}) => {
+  let hollow = true;
   let icon = null;
 
   switch (type) {
@@ -47,6 +58,9 @@ export const getIcon = ({type, label, className, standardIcon = false, presentat
       break;
     case icons.CLOSE:
       icon = Close;
+      break;
+    case icons.HOT:
+      icon = Hot;
       break;
     case icons.PHOTO:
       icon = Photo;
@@ -60,20 +74,22 @@ export const getIcon = ({type, label, className, standardIcon = false, presentat
     case icons.THREEDOTS:
       icon = ThreeDots;
       break;
+    case icons.USERS:
+      hollow = false;
+      icon = Users;
+      break;
     case icons.VIDEO:
       icon = Video;
       break;
     default:
       throw new Error(`Unsupported action link type '${type}'`);
   }
-
   const props = { className, hollow, icon, label, presentationOnly, standardIcon };
   return <SVG {...props} />;
 };
 
 getIcon.propTypes = {
   className: PropTypes.string,
-  hollow: PropTypes.bool,
   type: PropTypes.string.isRequired,
   label: PropTypes.string,
   presentationOnly: PropTypes.bool,
@@ -82,10 +98,7 @@ getIcon.propTypes = {
 
 getIcon.defaultProps = {
   className: null,
-  hollow: true,
   label: null,
   presentationOnly: false,
   standardIcon: false,
 };
-
-export default icons;
