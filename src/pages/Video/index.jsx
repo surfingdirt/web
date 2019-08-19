b7514009ab55dee2eaaa31233c9a6a9fc83a6670
Mikael Gramont <mgramont@gmail.com>
2019-08-19 11:33:41 +0200

    Render basic video pages

--- src/pages/Video/index.jsx
+++ src/pages/Video/index.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-import VIDEO from 'Apollo/queries/video2.gql';
+import VIDEO from 'Apollo/queries/video.gql';
 import DataRenderer from 'Components/DataRenderer';
+import Card, { cardTypes } from 'Components/Card';
+import Translate from 'Hocs/Translate';
+
 import { albumRoute, userRoute } from 'Utils/links';
 
+import messages from './messages';
 import styles from './styles.scss';
+import Paragraph from 'Components/Paragraph';
 
-export const Video = ({ match }) => {
+const { HERO } = cardTypes;
+
+const VideoRaw = ({ match, t }) => {
   const { id } = match.params;
 
   return (
@@ -17,19 +24,48 @@ export const Video = ({ match }) => {
       variables={{ id }}
       render={(data) => {
         const {
-          video: { title, embedUrl },
+          video: {
+            description,
+            embedUrl,
+            title,
+            submitter: { userId, username },
+            width,
+            height,
+          },
         } = data;
-        return (
-          <div className={styles.page}>
-            <h1>{title}</h1>
-            <iframe src={embedUrl} />
+
+        const wrapperStyle = {
+          paddingTop: `${(100 * height) / width}%`,
+        };
+        const videoStyle = {};
+
+        const heroContent = (
+          <div className={styles.videoWrapper} style={wrapperStyle}>
+            <iframe
+              className={styles.video}
+              style={videoStyle}
+              title={t('videoContent')}
+              src={embedUrl}
+            />
           </div>
+        );
+
+        return (
+          <Card title={title} type={HERO} heroContent={heroContent}>
+            <div>
+              {description && <Paragraph>{description}</Paragraph>}
+              <span>Posted by:</span>
+              <Link to={userRoute(userId)}>{username}</Link>
+            </div>
+          </Card>
         );
       }}
     />
   );
 };
 
-Video.propTypes = {
+VideoRaw.propTypes = {
   match: PropTypes.objectOf(PropTypes.any).isRequired,
 };
+
+export const Video = Translate(messages)(VideoRaw);
