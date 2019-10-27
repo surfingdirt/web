f3a487adccdfae7d17115b257d7c04de6dd61cf0
Mikael Gramont <mgramont@gmail.com>
2019-10-27 10:57:54 +0100

    MediaOverlay works: loads additional items when needed.

--- src/components/Media/MediaOverlay/index.jsx
+++ src/components/Media/MediaOverlay/index.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { useQuery } from '@apollo/react-hooks';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
+import LIST_MEDIA from 'Apollo/queries/listMedia.gql';
 import Card, { cardTypes } from 'Components/Card';
 import HeroContent from 'Components/Media/HeroContent';
 import MediaMetadata from 'Components/Media/MediaMetadata';
@@ -14,14 +16,20 @@ import messages from './messages';
 const { STANDARD } = cardTypes;
 const { NEXT, PREVIOUS } = icons;
 
+const PRELOAD_WHEN_REMAINING_FEWER_THAN = 2;
+const PAGINATION_ITEM_COUNT = 5;
+
 const MediaOverlay = ({ album, index: initialIndex, onTitleChange, t }) => {
   // TODO: display the initial media, fetch paginated lists of media
 
   const [items, setItems] = useState(album.media);
   const [item, setItem] = useState(album.media[initialIndex]);
   const [index, setIndex] = useState(initialIndex);
+  const [reachedEnd, setReachedEnd] = useState(false);
 
-  useEffect(() => {}, [items]);
+  useEffect(() => {
+    console.log('Detected change in items', items);
+  }, [items]);
 
   useEffect(() => {
     setItem(items[index]);
@@ -31,6 +39,18 @@ const MediaOverlay = ({ album, index: initialIndex, onTitleChange, t }) => {
   const lastIndex = items.length - 1;
   const hidePrev = index === 0;
   const hideNext = index === lastIndex;
+
+  const { error, fetchMore } = useQuery(LIST_MEDIA, {
+    variables: {
+      albumId: album.id,
+      startItem: 0,
+      countItems: PAGINATION_ITEM_COUNT,
+    },
+  });
+
+  if (error) {
+    console.error('Error while loading initial item list', error);
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -55,7 +75,44 @@ const MediaOverlay = ({ album, index: initialIndex, onTitleChange, t }) => {
           type="button"
           aria-label={t('next')}
           onClick={() => {
-            setIndex(index < lastIndex ? index + 1 : lastIndex);
+            const newIndex = index < lastIndex ? index + 1 : lastIndex;
+            setIndex(newIndex);
+            console.log({ newIndex });
+
+            if (reachedEnd) {
+              console.log('Would load more but end was reached');
+              return;
+            }
+
+            if (index >= lastIndex - PRELOAD_WHEN_REMAINING_FEWER_THAN) {
+              console.log('Loading more');
+              fetchMore({
+                variables: {
+                  albumId: album.id,
+                  startItem: lastIndex + 1,
+                  countItems: PAGINATION_ITEM_COUNT,
+                },
+                updateQuery: (prev, { fetchMoreResult }) => {
+                  if (!fetchMoreResult) {
+                    return prev;
+                  }
+                  if (fetchMoreResult.listMedia.length === 0) {
+                    console.log('No more items to load');
+                    setReachedEnd(true);
+                    return prev;
+                  }
+                  console.log({
+                    oldList: items,
+                    additionalList: fetchMoreResult.listMedia,
+                  });
+                  const newItems = [...items, ...fetchMoreResult.listMedia];
+                  console.log({ newItems });
+                  setItems(newItems);
+
+                  return newItems;
+                },
+              });
+            }
           }}
         >
           <div className={styles.buttonIcon}>{getIcon({ type: NEXT })}</div>
