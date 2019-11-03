d06734f72ff1625224faa23eda243c58ee372e23
Mikael Gramont <mgramont@gmail.com>
2019-11-03 10:34:56 +0100

    Cleanup

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -1,6 +1,5 @@
 export const focusFirstFocusableItemInside = (parentEl) => {
   // Find the first focusable item. Stick to links for now.
-  console.log(parentEl);
   const links = parentEl.getElementsByTagName('a');
   if (links && links.length >= 1) {
     links[0].focus();
@@ -9,7 +8,8 @@ export const focusFirstFocusableItemInside = (parentEl) => {
 
 export const uuidv4 = () => {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    const r = (Math.random() * 16) | 0,
+      v = c == 'x' ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
-}
+};
