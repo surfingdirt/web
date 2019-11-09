c4dbf9fb1198d2d95fde3f0b317ea74c96a3fd71
Mikael Gramont <mgramont@gmail.com>
2019-11-09 16:30:34 +0100

    Byebye storybook and your broken build. Call me maybe later.

--- .storybook/theme.js
+++
@@ -1,8 +0,0 @@
-import { create } from '@storybook/theming';
-
-export default create({
-  colorSecondary: '#6dcff6',
-  
-  brandTitle: 'Surfing Dirt UI',
-  brandUrl: 'https://www.surfingdirt.com/',
-});
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { createSerializer } from 'enzyme-to-json';
-import sinon from 'sinon';
 
 // eslint-disable-next-line no-undef
 expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
@@ -15,4 +14,3 @@ global.React = React;
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
-global.sinon = sinon;
