1480ef0fac42ab73942655f64faf6b7ca95a3552
Mikael Gramont <mgramont@gmail.com>
2019-11-22 16:41:59 +0100

    Refactored photo and video pages to use the same media GraphQL query.

--- src/pages/Photo/index.jsx
+++ src/pages/Photo/index.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import PHOTO from 'Apollo/queries/photo3.gql';
+import MEDIA from 'Apollo/queries/media2.gql';
 import DataRenderer from 'Components/Widgets/DataRenderer';
 import MediaPageContent from 'Components/Media/MediaPageContent';
 import { mediaTypes } from 'Utils/media';
 
-const { PHOTO: PHOTO_TYPE } = mediaTypes;
+const { PHOTO } = mediaTypes;
 
 export const Photo = ({ match }) => {
   const { id } = match.params;
 
   return (
     <DataRenderer
-      query={PHOTO}
-      variables={{ id }}
-      render={({ listComments, photo }) => {
-        const media = Object.assign({}, photo, { mediaType: PHOTO_TYPE });
-        return <MediaPageContent media={media} comments={listComments} />;
-      }}
+      query={MEDIA}
+      variables={{ id, mediaType: PHOTO.toLowerCase() }}
+      render={({ listComments, media }) => (
+        <MediaPageContent media={media} comments={listComments} />
+      )}
     />
   );
 };
--- src/pages/Video/index.jsx
+++ src/pages/Video/index.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import VIDEO from 'Apollo/queries/video3.gql';
+import MEDIA from 'Apollo/queries/media2.gql';
 import DataRenderer from 'Components/Widgets/DataRenderer';
 import MediaPageContent from 'Components/Media/MediaPageContent';
 import { mediaTypes } from 'Utils/media';
 
-const { VIDEO: VIDEO_TYPE } = mediaTypes;
+const { VIDEO } = mediaTypes;
 
 export const Video = ({ match }) => {
   const { id } = match.params;
 
   return (
     <DataRenderer
-      query={VIDEO}
-      variables={{ id }}
-      render={({ listComments, video }) => {
-        const media = Object.assign({}, video, { mediaType: VIDEO_TYPE });
+      query={MEDIA}
+      variables={{ id, mediaType: VIDEO.toLowerCase() }}
+      render={({ listComments, media }) => {
         return <MediaPageContent media={media} comments={listComments} />;
       }}
     />
