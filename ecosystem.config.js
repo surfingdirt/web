01584a3eb1e46cfe10ffa7259fbd94ec1a7578a6
Mikael Gramont <mgramont@gmail.com>
2020-01-18 10:42:54 +0100

    pm2 config changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,33 @@
 // pm2 start ecosystem.config.js --env beta
 
+const commonConfig = {
+  script: 'index.js',
+  error_file: 'log_error.log',
+  out_file: 'log_out.log',
+  log_file: 'log_combined.log',
+  time: true,
+
+  // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+  args: 'one two',
+  instances: 1,
+  autorestart: true,
+  watch: false,
+  max_memory_restart: '1G',
+};
+
 module.exports = {
   apps: [
-    {
-      name: 'Surfing Dirt - web server',
-      script: 'index.js',
-      error_file: 'log_error.log',
-      out_file: 'log_out.log',
-      log_file: 'log_combined.log',
-      time: true,
-
-      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-      args: 'one two',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
+    Object.assign({}, commonConfig, {
+      name: 'Surfing Dirt - web server - beta',
       env: {
-        NODE_ENV: 'development',
-      },
-      env_beta: {
         NODE_ENV: 'beta',
       },
-      env_production: {
+    }),
+    Object.assign({}, commonConfig, {
+      name: 'Surfing Dirt - web server - production',
+      env: {
         NODE_ENV: 'production',
       },
-    },
+    }),
   ],
 };
