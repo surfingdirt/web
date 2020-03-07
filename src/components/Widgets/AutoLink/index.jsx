65a6bf5394a8753c915aa4c09b6aa726bc18482c
Mikael Gramont <mgramont@gmail.com>
2020-03-07 15:31:49 +0100

    Fix broken autolinks when they contained ':'

--- src/components/Widgets/AutoLink/index.jsx
+++ src/components/Widgets/AutoLink/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // Inspired by https://github.com/banyan/react-autolink/blob/master/src/react-autolink.js
-const DELIMITER = /((?:https?:\/\/)?(?:(?:[a-z0-9]?(?:[a-z0-9\-]{1,61}[a-z0-9])?\.[^\.|\s])+[a-z\.]*[a-z]+|(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3})(?::\d{1,5})*[a-z0-9.,_\/~#&=;%+?\-\\(\\)]*)/gi;
+const DELIMITER = /((?:https?:\/\/)?(?:(?:[a-z0-9]?(?:[a-z0-9\-]{1,61}[a-z0-9])?\.[^\.|\s])+[a-z\.]*[a-z]+|(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3})(?::\d{1,5})*[a-z0-9.,:_\/~#&=;%+?\-\\(\\)]*)/gi;
 
 const strStartsWith = (str, prefix) => {
   return str.slice(0, prefix.length) === prefix;
