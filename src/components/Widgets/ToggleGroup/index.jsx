e73f79c64b68438e4f05e3ecaba5d196b20b6ca2
Mikael Gramont <mgramont@gmail.com>
2020-04-05 15:43:31 +0200

    ToggleGroup click updates url

--- src/components/Widgets/ToggleGroup/index.jsx
+++ src/components/Widgets/ToggleGroup/index.jsx
@@ -1,37 +1,40 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
 import styles from './styles.scss';
 
-const ToggleGroup = ({ items, name, onChange, selected }) => {
-  return (
-    <form
-      className={styles.wrapper}
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
-    >
-      <div className={styles.background}>
-        {items.map(({ icon, label, value }) => (
-          <button
-            className={classnames(styles.button, { [styles.selected]: selected === value })}
-            name={name}
-            key={value}
-            type="submit"
-            value={value}
-            aria-label={label}
-            onClick={() => {
-              onChange(value);
-            }}
-          >
-            {icon}
-          </button>
-        ))}
-      </div>
-    </form>
-  );
-};
+const ToggleGroup = ({ history, items, location, name, onChange, selected }) => (
+  <form
+    className={styles.wrapper}
+    onSubmit={(e) => {
+      e.preventDefault();
+    }}
+  >
+    <div className={styles.background}>
+      {items.map(({ icon, label, value }) => (
+        <button
+          className={classnames(styles.button, { [styles.selected]: selected === value })}
+          name={name}
+          key={value}
+          type="submit"
+          value={value}
+          aria-label={label}
+          onClick={() => {
+            const { search } = location;
+            const query = new URLSearchParams(search);
+            query.set(name, value);
+            history.replace(`?${query.toString()}`);
+            onChange(value);
+          }}
+        >
+          {icon}
+        </button>
+      ))}
+    </div>
+  </form>
+);
 
 ToggleGroup.propTypes = {
   items: PropTypes.arrayOf(
@@ -50,4 +53,4 @@ ToggleGroup.defaultProps = {
   name: 'toggle',
 };
 
-export default ToggleGroup;
+export default withRouter(ToggleGroup);
