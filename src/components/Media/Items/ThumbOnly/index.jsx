41d80fca982ce0d4627cfa5e5ffd195caa8ec092
Mikael Gramont <mgramont@gmail.com>
2020-04-14 08:42:35 +0200

    Fixing bug where ThumbOnly still had a modal. Whoops.

--- src/components/Media/Items/ThumbOnly/index.jsx
+++ src/components/Media/Items/ThumbOnly/index.jsx
@@ -1,58 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-import MediaOverlay from 'Components/Media/MediaOverlay';
 import MediaThumb from 'Components/Media/MediaThumb';
-import { modalTypes } from 'Components/Widgets/Modal';
 
-import WithModal from 'Hocs/WithModal';
-import Translate from 'Hocs/Translate';
-import { mediaPageSize } from 'Utils/media';
-import { AlbumType, MediaType } from 'Utils/types';
+import { MediaType } from 'Utils/types';
 
-import messages from './messages';
 import styles from './styles.scss';
 
-const { HERO } = modalTypes;
-const countItems = mediaPageSize;
-
-const ThumbOnly = ({ album, className, item, index, media, t }) => {
-  const {
-    title: { text: albumTitle },
-  } = album;
+const ThumbOnly = ({ className, item }) => {
   const {
     id,
     mediaType,
     title: { text: title },
     thumbs,
   } = item;
-  const attrs = { className: styles.link, id, mediaType, title, thumbs };
+  const attrs = { className: classnames(styles.link, className), id, mediaType, title, thumbs };
 
-  const ThumbWithModal = WithModal({
-    ariaLabel: t('mediaPreviewModal'),
-    className,
-    modalContent: (
-      <MediaOverlay album={album} media={media} countItems={countItems} index={index} />
-    ),
-    modalTitle: title || albumTitle,
-    tag: 'li',
-    type: HERO,
-  })(<MediaThumb {...attrs} objectFit />);
-
-  return <ThumbWithModal />;
+  return <MediaThumb {...attrs} objectFit />;
 };
 
 ThumbOnly.propTypes = {
-  album: AlbumType.isRequired,
   className: PropTypes.string,
-  index: PropTypes.number.isRequired,
   item: MediaType.isRequired,
-  media: PropTypes.arrayOf(PropTypes.shape()).isRequired,
-  t: PropTypes.func.isRequired,
 };
 
 ThumbOnly.defaultProps = {
   className: null,
 };
 
-export default Translate(messages)(ThumbOnly);
+export default ThumbOnly;
--- src/components/Media/Items/ThumbOnly/messages.js
+++
@@ -1,9 +0,0 @@
-const messages = (getText, getPlural) => (key) =>
-  ({
-    mediaPreviewModal: getText(
-      'Media preview',
-      'AlbumGrid',
-    ) /* Label for a window containing a quick preview of a media page */,
-  }[key]);
-
-export default messages;
