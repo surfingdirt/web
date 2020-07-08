2b555e61f375e792959937905d054c2374cefe74
Mikael Gramont <mgramont@gmail.com>
2020-07-08 08:14:30 +0200

    Added Firebase config

--- config/index.js
+++ config/index.js
@@ -5,10 +5,23 @@ export const fbAppId = '1726273124340058';
 export const fragmentTypesFile = './fragmentTypes.json';
 export const analyticsId = 'UA-84394442-2';
 
+// Firebase config. These can be seen by the public:
+// https://firebase.google.com/docs/projects/learn-more#config-files-objects
+const firebaseConfig = {
+  apiKey: 'AIzaSyCfmZ0NDvI_93HFY_8QIAYakXq04IkAxEA',
+  authDomain: 'surfing-dirt.firebaseapp.com',
+  databaseURL: 'https://surfing-dirt.firebaseio.com',
+  projectId: 'surfing-dirt',
+  storageBucket: 'surfing-dirt.appspot.com',
+  messagingSenderId: '219277862621',
+  appId: '1:219277862621:web:1f4c17dd4c665938d91d34',
+};
+
 export const config = {
   local: {
     port: 3033,
     baseUrl: `http://localhost:3033/`,
+    firebaseConfig,
     graphql: 'http://localhost:4000',
     galleryAlbumId: 'a3833b1c-1db0-4a93-9efc-b6659400ce9f',
     showErrors: true,
@@ -23,6 +36,7 @@ export const config = {
   beta: {
     port: 8002,
     baseUrl: `https://beta.surfingdirt.com`,
+    firebaseConfig,
     graphql: 'https://beta-graphql.surfingdirt.com/',
     galleryAlbumId: 'a3833b1c-1db0-4a93-9efc-b6659400ce9f',
     showErrors: false,
@@ -37,6 +51,7 @@ export const config = {
   production: {
     port: 8001,
     baseUrl: `https://www.surfingdirt.com`,
+    firebaseConfig,
     graphql: 'https://graphql.surfingdirt.com/',
     galleryAlbumId: 'a3833b1c-1db0-4a93-9efc-b6659400ce9f',
     showErrors: false,
