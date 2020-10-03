16198a1f2c1d3b89888abc5c95bdfeb09c76f4a8
Mikael Gramont <mgramont@gmail.com>
2020-10-03 10:58:07 +0200

    Local points to prod GraphQL

--- config/index.js
+++ config/index.js
@@ -22,7 +22,8 @@ export const config = {
     port: 3033,
     baseUrl: `http://localhost:3033/`,
     firebaseConfig,
-    graphql: 'http://localhost:4000',
+    // graphql: 'http://localhost:4000',
+    graphql: 'https://graphql.surfingdirt.com/',
     galleryAlbumId: 'a3833b1c-1db0-4a93-9efc-b6659400ce9f',
     showErrors: true,
     tracing: {
